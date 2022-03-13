073c697a72c2a5a1fc6cb9895948d1865610e755
Sri Manokaran <smanokaran@student.unimelb.edu.au>
2022-03-13 12:47:09 +1100

    Rendered elements to board

--- app.js
+++ app.js
@@ -1,16 +1,43 @@
-// first cell
-let cross = document.createElement('cross');
-cross.className = "symbol";
-cross.innerHTML = "<span id='x'> X </span> ";
+// Create cross element
+function get_cross(){
+    let cross = document.createElement('cross');
+    cross.className = "symbol";
+    cross.innerHTML = "<div id='x'> X </div> ";
+    return cross;
+}
 
-let nought = document.createElement('nought');
-nought.className = "symbol";
-nought.innerHTML = "<span id='o'> O </span>"
 
-const tbody = document.querySelector('#board tbody');
-tbody.addEventListener('click', function (e) {
-  const cell = e.target.closest('td');
-  if (!cell) {return;} // Quit, not clicked on a cell
-  const row = cell.parentElement;
-  cell.append(cross);   
-});
+// Create nought element
+function get_nought(){
+    let nought = document.createElement('nought');
+    nought.className = "symbol";
+    nought.innerHTML = "<span id='o'> O </div>";
+    return nought;
+}
+
+function sign_turns(count){
+    if(count % 2 == 0){
+        return get_cross();
+    }
+    return get_nought();
+}
+
+let count = 0;
+let cell = document.querySelectorAll("#board td");
+for(let i = 0; i < cell.length; i++) {
+    cell[i].addEventListener("click", function(){
+
+        if(!cell[i].hasChildNodes()){
+            // replace with choice of sign later
+            cell[i].append(sign_turns(count));
+            count++;
+        
+            // if (document.querySelectorAll("#board td")[i].getElementsByTagName("nought").length == 1 ){
+            //     console.log("Yes");
+            // }
+            // console.log(cell[i].getAttribute("id"));
+            
+        }
+    });
+}
+
