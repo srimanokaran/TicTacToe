369caf3611e5c537d75401c97cdef1fe53ee9d00
Sri Manokaran <smanokaran@student.unimelb.edu.au>
2022-03-14 17:49:17 +1100

    Updated tie functionality

--- app.js
+++ app.js
@@ -38,64 +38,65 @@ function check_win(counter, board){
 
     //row cross
     if(board[0][0] == cross && board[0][1] == cross && board[0][2] == cross){
-        win_message("cross");
+        message("cross");
     }
     if(board[1][0] == cross && board[1][1] == cross && board[1][2] == cross){
-        win_message("cross");
+        message("cross");
     }
     if(board[2][0] == cross && board[2][1] == cross && board[2][2] == cross){
-        win_message("cross");
+        message("cross");
     }
 
     //row nought
     if(board[0][0] == nought && board[0][1] == nought && board[0][2] == nought){
-        win_message("nought");
+        message("nought");
     }
     if(board[1][0] == nought && board[1][1] == nought && board[1][2] == nought){
-        win_message("nought");
+        message("nought");
     }
     if(board[2][0] == nought&& board[2][1] == nought && board[2][2] == nought){
-        win_message("nought");
+        message("nought");
     }
 
     //column cross
     if(board[0][0] == cross && board[1][0] == cross && board[2][0] == cross){
-        win_message("cross");
+        message("cross");
     }
     if(board[0][1] == cross && board[1][1] == cross && board[2][1] == cross){
-        win_message("cross");
+        message("cross");
     }
     if(board[0][2] == cross && board[1][2] == cross && board[2][2] == cross){
-        win_message("cross");
+        message("cross");
     }
     
     //column nought
     if(board[0][0] == nought && board[1][0] == nought && board[2][0] == nought){
-        win_message("nought");
+        message("nought");
     }
     if(board[0][1] == nought && board[1][1] == nought && board[2][1] == nought){
-        win_message("nought");
+        message("nought");
     }
     if(board[0][2] == nought && board[1][2] == nought && board[2][2] == nought){
-        win_message("nought");
+        message("nought");
     }
 
     //diagonal
     if(board[0][0] == cross && board[1][1] == cross && board[2][2] == cross){
-        win_message("cross");
+        message("cross");
     }
 
     if(board[0][2] == cross && board[1][1] == cross && board[2][0] == cross){
-        win_message("cross");
+        message("cross");
     }
 
     if(board[0][0] == nought && board[1][1] == nought && board[2][2] == nought){
-        win_message("nought");
+        message("nought");
     }
 
     if(board[0][2] == nought && board[1][1] == nought && board[2][0] == nought){
-        win_message("nought");
+        message("nought");
     }
+    
 }
 
 function get_row(counter){
@@ -135,7 +136,6 @@ function play_game(){
     let cell = document.querySelectorAll("#board td");
     for(let i = 0; i < cell.length; i++) {
         cell[i].addEventListener("click", function(){
-
             if(!cell[i].hasChildNodes()){
                 // TODO: replace with choice of sign 
                 cell[i].append(turn_change(count));
@@ -144,16 +144,16 @@ function play_game(){
                 //check to see if there is a win
                 check_win(i, board);
                 
+                if(count == cell.length) message("tie");
             }
-        });
+        });   
     }
 }
 
-function win_message(symbol){
+function message(symbol){
 
     if(symbol === "cross"){
 
-        
         Swal.fire({
             icon: 'success',
             text: 'Cross wins!'
@@ -161,7 +161,7 @@ function win_message(symbol){
         });
         
     }
-    else{
+    else if(symbol === "nought"){
         Swal.fire({
             icon: 'success',
             text: 'Nought wins!'
@@ -169,10 +169,17 @@ function win_message(symbol){
         });
     }
 
+    else{
+        Swal.fire({
+            icon: 'info',
+            text: 'Tie!'
+        });
+    }
+
+    //reload page once user clicks ok
     document.getElementsByClassName("swal2-confirm swal2-styled")[0].addEventListener("click", function(){
         location.reload();
     });
 }
-
 
 play_game();
